ed978a48669fa0bdae72fcb964a2a362146afb7e
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-03-31 18:33:11 +0530

    styling the transaction widget

--- lib/widgets/transaction.dart
+++ lib/widgets/transaction.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 
 class Transaction extends StatelessWidget {
-
   final TransactionModel transaction;
 
   Transaction({this.transaction});
@@ -12,11 +11,50 @@ class Transaction extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Container(
-        margin: EdgeInsets.all(16),
         width: double.infinity,
-        child: Text(
-          transaction.text,
-          textDirection: TextDirection.ltr,
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 20,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.orange,
+                  width: 2,
+                ),
+              ),
+              padding: EdgeInsets.all(10),
+              child: Text(
+                transaction.amount.toString(),
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.orange),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  transaction.text,
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  transaction.date.toString(),
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 12
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
